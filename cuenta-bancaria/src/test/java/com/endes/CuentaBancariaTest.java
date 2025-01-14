ebb8ee298c0b8086088daac949528ee954c8474f
Pablo <pablo.dominguez@iescamas.es>
2025-01-14 12:46:53 +0100

    añadido test constructor cuentabancari

--- cuenta-bancaria/src/test/java/com/endes/CuentaBancariaTest.java
+++ cuenta-bancaria/src/test/java/com/endes/CuentaBancariaTest.java
@@ -6,29 +6,34 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CuentaBancariaTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
+	CuentaBancaria cuenta;
 	@BeforeEach
-	void setUp() throws Exception {
+	void setup() throws Exception{
+		cuenta = new CuentaBancaria(1000);
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
+	/**
+	 * Pruebas del constructor
+	 */
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Prueba donde el saldo inicial es correcto")
+	void testConstructorValido() {
+		double resultadoEsperado = 1000.0;
+		assertEquals(resultadoEsperado, cuenta.getSaldo(), "No se corresponde el saldo obtenido con el pasado al constructor");
+	}
+	
+	@Test
+	@DisplayName("Prueba donde el saldo incial es negativo")
+	void testConstructorNoValido() {
+		String mensajeEsperado = "El saldo inicial no puede ser negativo.";
+		Exception exception = assertThrows(IllegalArgumentException.class, ()-> new CuentaBancaria(-200));
+		
+		assertEquals(mensajeEsperado, exception.getMessage(), "Deben coincidir las respuestas al crear la cuenta con un saldo negativo");
 	}
 
 }
